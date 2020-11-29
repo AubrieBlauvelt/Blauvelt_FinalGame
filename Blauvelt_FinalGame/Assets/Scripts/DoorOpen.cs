f8f2a8acfc59355cdf6fdacb8cbd7f1d16a6a644
Aubrie Blauvelt <aublauve@iu.edu>
2020-11-28 22:04:35 -0500

    Door Script
    started a script for functioning doors

---
+++ Blauvelt_FinalGame/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public bool open;
+    public bool close;
+    public bool inTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (close)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    open = true;
+                    close = false;
+                }
+            }
+            else
+            {
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    close = true;
+                    open = false;
+                }
+            }
+        }
+        if (open)
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot; //tells door how much too rotate upon opening
+        }
+        else
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+    }
+
+    void OnGUI()
+    {
+        if(inTrigger)
+        {
+            if(open)
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Press E to close");
+            }
+            else
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Press E to open");
+            }
+        }
+    }
+}
+
+  
