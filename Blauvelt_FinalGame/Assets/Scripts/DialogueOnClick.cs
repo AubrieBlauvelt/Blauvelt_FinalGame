2d6eefbee81f29a268f368b14d24c46cc7c39c01
Aubrie Blauvelt <aublauve@iu.edu>
2020-11-30 20:48:26 -0500

    Text Box Script
    Created Script for text boxes

---
+++ Blauvelt_FinalGame/Assets/Scripts/DialogueOnClick.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueOnClick : MonoBehaviour
+{
+
+    public string dialogueToDisplay;
+
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+
+    bool dialogueOpened;
+    bool canClose = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (dialogueOpened)
+            {
+                if (!canClose)
+                {
+                    canClose = true;
+                }
+                else
+                    CloseDialoguePanel();
+            }
+        }
+
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (!dialogueOpened)
+            OpenDialoguePanel();
+    }
+
+    void OpenDialoguePanel()
+    {
+        dialogueOpened = true;
+        Time.timeScale = 0f;
+
+        dialoguePanel.SetActive(true);
+        dialogueText.text = dialogueToDisplay;
+
+        canClose = false;
+    }
+
+    void CloseDialoguePanel()
+    {
+        dialogueOpened = false;
+        Time.timeScale = 1f;
+
+        dialoguePanel.SetActive(false);
+    }
+}
