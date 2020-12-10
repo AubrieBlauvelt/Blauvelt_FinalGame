2a00fd3a19ab1cbf88f094b603520967293fa048
Aubrie Blauvelt <aublauve@iu.edu>
2020-12-10 17:28:55 -0500

    Fixed Pickup Script
    now the ability to pick up and move objects is waaaaaaay smoother.

--- Blauvelt_FinalGame/Assets/Scripts/PickUp.cs
+++ Blauvelt_FinalGame/Assets/Scripts/PickUp.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     public Transform theDest;
+    public GameObject tempParent;
+    public Transform destination;
+
+    void start ()
+    {
+        this.GetComponent<Rigidbody>().useGravity = true;
+    }
 
     void OnMouseDown()
     {
-        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
         GetComponent<Rigidbody>().useGravity = false;
         this.transform.position = theDest.position;
-        this.transform.parent = GameObject.Find("Destination").transform;
+        this.transform.rotation = theDest.transform.rotation;
+        this.transform.parent = tempParent.transform;
     }
 
      void OnMouseUp()
     {
         this.transform.parent = null;
         GetComponent<Rigidbody>().useGravity = true;
-        GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        this.transform.parent = null;
+        this.transform.position = theDest.transform.position; 
     }
 }
---
+++ Blauvelt_FinalGame/Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerZone : MonoBehaviour
+{
+    public Text MyText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyText.text = "" + score; 
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag ("Pickup"))
+        {
+            score = score + 1;
+        }
+    }
+}
